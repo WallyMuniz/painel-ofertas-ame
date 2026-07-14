1551f14a24b575fcacfb5c4053305de37b6c9fb8
WallyMuniz <135264339+WallyMuniz@users.noreply.github.com>
2026-07-14 09:56:49 -0300

    Atualiza painel público: js/painel-rotas-page.js

--- js/painel-rotas-page.js
+++ js/painel-rotas-page.js
@@ -53,8 +53,8 @@
   const numberFormatter = new Intl.NumberFormat("pt-BR", { maximumFractionDigits: 0 });
   const percentFormatter = new Intl.NumberFormat("pt-BR", {
     style: "percent",
-    minimumFractionDigits: 1,
-    maximumFractionDigits: 1,
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
   });
 
   const elements = {
